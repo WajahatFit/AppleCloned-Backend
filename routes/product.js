c4fd227347dc0601d55aeaac13d3dc1d10154e02
Syed Wajahat Ali <wajahat.klk25.com>
2022-09-06 21:07:16 +0200

    routes merges into dev

---
+++ routes/product.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const ErrorResponse = require('../utils/error');
+const Product = require('../models/Product');
+
+
+// @desc    GET all the products
+// @route   GET /api/v1/
+// @access  Public
+router.get('/', async (req, res, next) => {
+  try {
+    const product = await Product.find({});
+    if (!product) {
+      next(new ErrorResponse('No products found', 404));
+    }
+    res.status(200).json({ data: product })
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc    Get single project
+// @route   GET /:id
+// @access  Public
+
+router.get('/:id', async (req, res, next) => {
+  const {id} = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      next(new ErrorResponse(`No product found by id: ${id}`, 404));
+    }
+    res.status(200).json({ data: product })
+  } catch (error) {
+    next(error);
+  }
+});
+
+// @desc    Create a product
+// @route   POST /
+// @access  Private
+
+// add auth here idk
+
+router.post('/create', async (req, res, next) => {
+  const { title, description, price, color, newStock } = req.body;
+  try {
+    const product = Product.create({title, description, price: parseInt(price), color, newStock});
+    if (!product) {
+      next(new ErrorResponse('An error ocurred while creating the project', 500));
+    }
+    res.status(201).json({ data: product })
+  } catch (error) {
+      next(error);
+  }
+});
+
+// @desc    Edit a product
+// @route   PUT /:id
+// @access  Public
+
+route.put('/edit', (req, res, next) =>{
+  const { title, description, price, color, newStock } = req.body;
+  const {id} = req.params;
+  try {
+    const product = Product.findById(id);
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+
+
+
+
+
+module.exports = router;
