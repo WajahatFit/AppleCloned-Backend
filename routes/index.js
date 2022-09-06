270940b3caa74056947340127bf1037b3799475d
Syed Wajahat Ali <wajahat.klk25.com>
2022-09-06 19:55:37 +0200

    readme updated

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 const router = require('express').Router();
 const ErrorResponse = require('../utils/error');
 
+// @desc    GET all the products
+// @route   GET /api/v1/
+// @access  Public
+router.get('/', async (req, res, next) => {
+  try {
+    const projects = await Project.find({});
+    if (!projects) {
+      next(new ErrorResponse('No projects found', 404));
+    }
+    res.status(200).json({ data: projects })
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 // @desc    GET all the projects
 // @route   GET /api/v1/
 // @access  Public
 router.get('/', async (req, res, next) => {
   res.send('REST API')
 });
+
 
 module.exports = router;
